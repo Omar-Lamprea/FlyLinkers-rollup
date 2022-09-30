3ac7d02f93a05eb63e244cd1f2b20cdbfda8caff
Omar-Lamprea <lampreaomar@gmail.com>
2022-09-30 15:09:58 -0500

    restored translate system

--- src/js/googleTranslate.js
+++ src/js/googleTranslate.js
@@ -1,7 +1,8 @@
 export const googleTranslateJs = async (text) =>{
   try {
     if (text) {
-      const API_KEY = 'AIzaSyBLxDf_fbc_6Yvst0Z0nLSPIW52J2dFbuc'
+      // const API_KEY = 'AIzaSyBLxDf_fbc_6Yvst0Z0nLSPIW52J2dFbuc'
+      const API_KEY = 'AIzaSyAw0uECgfavfakbO_S1JRUi-n9SaKvi7Mo'
       let res = await axios.post(`https://translation.googleapis.com/language/translate/v2?key=${API_KEY}`,{
         q: text,
         target: localStorage.getItem('lang')
